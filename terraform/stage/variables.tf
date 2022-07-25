8365e50ac1f1d19518e1815f4df7e5e552d1226d
Kirill Khapinskiy <hapi@bk.ru>
2022-07-26 03:34:19 +0900

    terraform-2fix 4

--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -1,32 +1,28 @@
-variable "cloud_id" {
+variable cloud_id {
   description = "Cloud"
 }
-variable "folder_id" {
+variable folder_id {
   description = "Folder"
 }
-variable "zone" {
+variable zone {
   description = "Zone"
   # Значение по умолчанию
   default = "ru-central1-a"
 }
-variable "public_key_path" {
+variable public_key_path {
   # Описание переменной
   description = "Path to the public key used for ssh access"
 }
-variable "image_id" {
-  description = "Disk image"
+variable private_key_path {
+  description = "path to private key"
 }
-variable "subnet_id" {
+variable image_id {
+  description = "Disk image"
+  default = "fd8ttaa2he5dc216i888"
+}
+variable subnet_id {
   description = "Subnet"
 }
-variable "service_account_key_file" {
+variable service_account_key_file {
   description = "key.json"
-}
-variable "private_key_path" {
-  # Описание переменной
-  description = "Path to the privat key used for ssh access"
-}
-variable "app_disk_image" {
-  description = "Disk image for reddit app"
-  default     = "reddit-app-base"
 }
