afb639165b260b464a82f8ec01b1ba9dcd7ff9f1
Kirill Khapinskiy <hapi@bk.ru>
2022-07-26 03:40:01 +0900

    terraform-2fix 4

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,5 +1,5 @@
 provider "yandex" {
-  service_account_key_file = var.account_key_path
+
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.zone
